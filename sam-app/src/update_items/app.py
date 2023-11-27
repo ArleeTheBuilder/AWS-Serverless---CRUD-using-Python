618b49b81606db40a1829fd4ffa7bbda476a151a
ArleeTheBuilder <arleethebuilder163@gmail.com>
2023-11-27 21:10:59 +0800

    Added update API

---
+++ sam-app/src/update_items/app.py
@@ -0,0 +1,49 @@
+import boto3
+import os
+import json
+import uuid
+from datetime import datetime
+
+def lambda_handler(message, context):
+
+    if ('body' not in message or
+            message['httpMethod'] != 'PUT'):
+        
+        return {
+            'statusCode': 400,
+            'headers': {},
+            'body': json.dumps({'msg': 'Bad Request'})
+        }
+
+    table_name = os.environ.get('TABLE', 'Items')
+    region = os.environ.get('REGION', 'us-east-1')
+
+    item_table = boto3.resource(
+        'dynamodb',
+        region_name=region   
+    )
+
+    table = item_table.Table(table_name)
+    activity = json.loads(message['body'])
+    item_id = message['pathParameters']['id']
+
+
+    params = {
+        'id': item_id
+    }
+
+    response = table.update_item(
+        Key=params,
+        ExpressionAttributeValues={
+            ":s": activity["itemName"],
+            ":s": activity["description"],
+            ":n": activity["price"],
+            ":s": activity["isActive"]
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(response['Items'])
+    }
